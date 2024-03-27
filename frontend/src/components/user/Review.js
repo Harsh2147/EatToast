d297af995c7bca9647cd75faf71f5dcd9e0d9f99
Mandeep Kaur <Mkaur6254@conestogac.on.ca>
2024-03-27 12:24:36 -0400

    commit

--- frontend/src/components/user/Review.js
+++ frontend/src/components/user/Review.js
@@ -91,146 +91,132 @@ function Review() {
       <Header />
       {/* header section end here */}
 
-      
-
       <div class="book-table section bg-light">
-                <div class="book-table-shape">
-                    <img src="/img/table-leaves-shape.png" alt="" />
-                </div>
+        <div class="book-table-shape">
+          <img src="/img/table-leaves-shape.png" alt="" />
+        </div>
 
-                <div class="book-table-shape book-table-shape2">
-                    <img src="/img/table-leaves-shape.png" alt="" />
-                </div>
+        <div class="book-table-shape book-table-shape2">
+          <img src="/img/table-leaves-shape.png" alt="" />
+        </div>
 
-                <div class="sec-wp">
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="sec-title text-center ">
-                                <h1 class="special-head text-center">
-                                    {" "}
-                                    GIVE <span>REVIEW</span>
-                                </h1>
-                                <p class="sec-sub-para">Your review matters a lot in your business .</p>
-                                <h2 class="section-break mx-auto w-50"></h2>
-
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-
-     
-      <div class="oder">
-        <div class="oder_main">
-    
-            <div class="form-row row">
-              <div class="form-container">
-                <form class="checkout-form" onSubmit={handleSubmit}>
-                
-
-                  <div class="row">
-                    <h3>Personal Information :</h3>
-                    <div class="col">
-                      <label for="first-name"> First Name</label>
-
-                      <input
-                        id="Firstname"
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter First name"
-                        name="Firstname"
-                        value={Firstname}
-                        onChange={(e) => setFirstname(e.target.value)}
-                      />
-                    </div>
-
-                    <div class="col">
-                      <label for="first-name"> Last Name</label>
-                      <input
-                        id="Lastname"
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter Last name"
-                        name="Lastname"
-                        value={Lastname}
-                        onChange={(e) => setLastname(e.target.value)}
-                      />
-                    </div>
-
-                    <div class="col">
-                      <label for="pnumber"> Phone number</label>
-
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter phone number"
-                        id="Mobile"
-                        name="Mobile"
-                        value={Mobile}
-                        onChange={(e) => setMobile(e.target.value)}
-                      />
-                    </div>
-
-                    <div class="col">
-                      <label for="email"> Email</label>
-
-                      <input
-                        type="Email"
-                        className="form-control"
-                        placeholder="Enter Email"
-                        id="email"
-                        name="email"
-                        value={email}
-                        onChange={(e) => setemail(e.target.value)}
-                      />
-                    </div>
-                  </div>
-
-                 
-
-                  <h2 class="section-break"></h2>
-
-                  <div class="row">
-                    <h3>Add Information :</h3>
-                    <div class="col">
-                      {/* <label for="order-details"> Order Details</label> */}
-                      <textarea
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter Message"
-                        id="Message"
-                        name="Message"
-                        value={Message}
-                        onChange={(e) => setMessage(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                  {errorMessages.length > 0 && (
-                    <div style={{ color: "red", fontWeight: "700" }}>
-                      <ul>
-                        {errorMessages.map((error, index) => (
-                          <li key={index}>{error}</li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
-                  <button type="submit" class="submit-button mt-3">
-                    SUBMIT REVIEW
-                  </button>
-                </form>
+        <div class="sec-wp">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="sec-title text-center ">
+                <h1 class="special-head text-center">
+                  {" "}
+                  GIVE <span>REVIEW</span>
+                </h1>
+                <p class="sec-sub-para">
+                  Your review matters a lot in your business .
+                </p>
+                <h2 class="section-break mx-auto w-50"></h2>
               </div>
             </div>
           </div>
         </div>
       </div>
 
+      <div class="oder">
+        <div class="oder_main">
+          <div class="form-row row">
+            <div class="form-container">
+              <form class="checkout-form" onSubmit={handleSubmit}>
+                <div class="row">
+                  <h3>Personal Information :</h3>
+                  <div class="col">
+                    <label for="first-name"> First Name</label>
 
+                    <input
+                      id="Firstname"
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter First name"
+                      name="Firstname"
+                      value={Firstname}
+                      onChange={(e) => setFirstname(e.target.value)}
+                    />
+                  </div>
+
+                  <div class="col">
+                    <label for="first-name"> Last Name</label>
+                    <input
+                      id="Lastname"
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Last name"
+                      name="Lastname"
+                      value={Lastname}
+                      onChange={(e) => setLastname(e.target.value)}
+                    />
+                  </div>
+
+                  <div class="col">
+                    <label for="pnumber"> Phone number</label>
+
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter phone number"
+                      id="Mobile"
+                      name="Mobile"
+                      value={Mobile}
+                      onChange={(e) => setMobile(e.target.value)}
+                    />
+                  </div>
+
+                  <div class="col">
+                    <label for="email"> Email</label>
+
+                    <input
+                      type="Email"
+                      className="form-control"
+                      placeholder="Enter Email"
+                      id="email"
+                      name="email"
+                      value={email}
+                      onChange={(e) => setemail(e.target.value)}
+                    />
+                  </div>
+                </div>
+
+                <h2 class="section-break"></h2>
+
+                <div class="row">
+                  <h3>Add Information :</h3>
+                  <div class="col">
+                    {/* <label for="order-details"> Order Details</label> */}
+                    <textarea
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Message"
+                      id="Message"
+                      name="Message"
+                      value={Message}
+                      onChange={(e) => setMessage(e.target.value)}
+                    />
+                  </div>
+                </div>
+                {errorMessages.length > 0 && (
+                  <div style={{ color: "red", fontWeight: "700" }}>
+                    <ul>
+                      {errorMessages.map((error, index) => (
+                        <li key={index}>{error}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+                <button type="submit" class="submit-button mt-3">
+                  SUBMIT REVIEW
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div class="review">
-
         <h1 class="text-center">Our Review</h1>
         <p class="text-center">
           Thanks for giving us a good review....We really appreciate it.
@@ -238,111 +224,17 @@ function Review() {
 
         <div class="review_line_1"></div>
         <div class="review_box">
-
-          <div class="review_card">
-            <div class="review_tag">
-              <h2>Name</h2>
-              <p class="info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusantium cupiditate deserunt odio in fugiat dolore!
-                Veniam sit quod iusto quas eligendi. Natus numquam
-                aspernatur alias illo voluptates dolorem, id ad.
-              </p>
-              <button class="submit-button">Raed more</button>
-            </div>
-          </div>
-          <div class="review_card">
-            <div class="review_tag">
-              <h2>Name</h2>
-              <p class="info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusantium cupiditate deserunt odio in fugiat dolore!
-                Veniam sit quod iusto quas eligendi. Natus numquam
-                aspernatur alias illo voluptates dolorem, id ad.
-              </p>
-              <button class="submit-button">Raed more</button>
-            </div>
-          </div>
-          <div class="review_card">
-            <div class="review_tag">
-              <h2>Name</h2>
-              <p class="info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusantium cupiditate deserunt odio in fugiat dolore!
-                Veniam sit quod iusto quas eligendi. Natus numquam
-                aspernatur alias illo voluptates dolorem, id ad.
-              </p>
-              <button class="submit-button">Raed more</button>
-            </div>
-          </div>
-          <div class="review_card">
-            <div class="review_tag">
-              <h2>Name</h2>
-              <p class="info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusantium cupiditate deserunt odio in fugiat dolore!
-                Veniam sit quod iusto quas eligendi. Natus numquam
-                aspernatur alias illo voluptates dolorem, id ad.
-              </p>
-              <button class="submit-button">Raed more</button>
-            </div>
-          </div>
-          
-
-          <div class="review_card">
-            <div class="review_tag">
-              <h2>Name</h2>
-              <p class="info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusantium cupiditate deserunt odio in fugiat dolore!
-                Veniam sit quod iusto quas eligendi. Natus numquam
-                aspernatur alias illo voluptates dolorem, id ad.
-              </p>
-              <button class="submit-button">Raed more</button>
-            </div>
-          </div>
-
-          <div class="review_card">
-            <div class="review_tag">
-              <h2>Name</h2>
-              <p class="info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusantium cupiditate deserunt odio in fugiat dolore!
-                Veniam sit quod iusto quas eligendi. Natus numquam
-                aspernatur alias illo voluptates dolorem, id ad.
-              </p>
-              <button class="submit-button">Raed more</button>
-            </div>
-          </div>
-
-          <div class="review_card">
-            <div class="review_tag">
-              <h2>Name</h2>
-              <p class="info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusantium cupiditate deserunt odio in fugiat dolore!
-                Veniam sit quod iusto quas eligendi. Natus numquam
-                aspernatur alias illo voluptates dolorem, id ad.
-              </p>
-              <button class="submit-button">Raed more</button>
-            </div>
-          </div>
-
-          <div class="review_card">
-            <div class="review_tag">
-              <h2>Name</h2>
-              <p class="info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Accusantium cupiditate deserunt odio in fugiat dolore!
-                Veniam sit quod iusto quas eligendi. Natus numquam
-                aspernatur alias illo voluptates dolorem, id ad.
-              </p>
-              <button class="submit-button">Raed more</button>
-            </div>
-          </div>
-          
-          
-
+          {data?.getAllReview_db.map((review) => (
+            <tr key={review._id}>
+              <div class="review_card">
+                <div class="review_tag">
+                  <h2>{review.Firstname}</h2>
+                  <p class="info">{review.Message}</p>
+                  <button class="submit-button">Raed more</button>
+                </div>
+              </div>
+            </tr>
+          ))}
         </div>
       </div>
     </>
